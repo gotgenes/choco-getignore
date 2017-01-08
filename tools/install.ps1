d0cdec803035b2e4d416a3b8c94214ccaf0b88ef
Ian Firkin <ian.firkin@gisinc.com>
2017-01-08 10:19:45 -0500

    Renames again, and fixes download URL
    * Switches back to correct default for installer
    * Fixes download URL

--- tools/install.ps1
+++
@@ -1,6 +0,0 @@
-﻿Install-ChocolateyZipPackage `
- -PackageName 'getignore' `
- -Checksum 179B9930F56E7B6168C37A13CB4F4761C4B14C420AB9559061D30380F0E53C59 `
- -ChecksumType sha256 `
- -Url 'https://github.com/gotgenes/getignore/releases/download/0.1.0/getignore-0.2.0-windows-amd64.zip' `
- -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
