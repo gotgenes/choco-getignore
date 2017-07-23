b36e6a65845fb8f1751e938f6f7eebdf1bac2b25
Chris Lasher <chris.lasher@gmail.com>
2017-07-23 10:54:16 -0700

    Provide 32-bit and 64-bit package information

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,9 @@
 ﻿Install-ChocolateyZipPackage `
  -PackageName 'getignore' `
- -Checksum 'fcd992e4df31a71c6cf8de39b8dbc4e36e91db782b2f6529893729ccc6a812cb' `
+ -Checksum '02c92b5ba75cfc10b620671f6642a9e19217ea6fcbeb8985c4cc60bbb5ac94cc' `
  -ChecksumType 'sha256' `
- -Url 'https://github.com/gotgenes/getignore/releases/download/1.0.0/getignore-1.0.0-windows-amd64.zip' `
+ -Url 'https://github.com/gotgenes/getignore/releases/download/1.0.0/getignore-1.0.0-windows-386.zip' `
+ -Checksum64 'fcd992e4df31a71c6cf8de39b8dbc4e36e91db782b2f6529893729ccc6a812cb' `
+ -Checksum64Type 'sha256' `
+ -Url64 'https://github.com/gotgenes/getignore/releases/download/1.0.0/getignore-1.0.0-windows-amd64.zip' `
  -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
